e385d93202b359b9831291d4b2e8457c3efb988c
snakebot2 <108169911+snakebot2@users.noreply.github.com>
2022-11-15 19:19:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = environ.get('AUTH_CHANNEL', '-1001818076982')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Punjabi123@cluster0.gwppywa.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Punjabi123:punjabi123@cluster0.gwppywa.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Punjabi")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Punjabi_files')
 
